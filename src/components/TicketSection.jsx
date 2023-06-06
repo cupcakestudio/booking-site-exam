f7a95c1d52b198f4b294b0905c94830be57b97fd
Ting Hua Lin <coolting.lin3@gmail.com>
2023-06-06 16:39:57 +0200

    add DatePicker component to ticketSection,  it parses correct value to conText

--- src/components/TicketSection.jsx
+++ src/components/TicketSection.jsx
@@ -3,19 +3,45 @@ import { InputLabel, TextField, Select, MenuItem } from "@mui/material";
 import styles from "../styles/Form.module.css";
 import { formDataContext } from "@/contexts/bookingContext";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+// import { startOfDay } from "date-fns";
 export default function TicketsSection() {
   //context call for the child component
   const { formData, dispatch } = useContext(formDataContext);
-
+  const minDate = new Date("2023-06-05");
+  // const maxDate = new const today = dayjs();
+  const today = dayjs();
   return (
     <>
       <h2 className={styles.h2}>Tickets</h2>
-      <TextField
-        className={styles.inputField}
-        type="date"
-        helperText="Choose a date"
+      <div className="datePickerContainer" style={styles.inputContainer}>
+        {/* <TextField label="Choose a date"></TextField> */}
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Controlled picker"
+            value={formData.date}
+            defaultValue={today}
+            views={["year", "month", "day"]}
+            onChange={(e) => {
+              const formattedDate = e.toISOString().split("T")[0];
+              dispatch({
+                //dispatch to the global formData obj. with new state value
+                action: "UPDATE_FIELD",
+                payload: { field: "date", value: formattedDate },
+              });
+            }}
+            renderInput={(params) => (
+              <TextField {...params} value={formData.date} />
+            )}
+          />
+        </LocalizationProvider>
+      </div>
+      {/* <DatePicker
+        label="Choose a date"
         value={formData.date}
-        required
         onChange={(e) =>
           dispatch({
             //dispatch to the global formData obj. with new state value
@@ -23,8 +49,18 @@ export default function TicketsSection() {
             payload: { field: "date", value: e.target.value },
           })
         }
-      />
-      <br></br>{" "}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            className={styles.inputField}
+            helperText="Choose a date"
+            required
+          />
+        )}
+        minDate={minDate}
+        maxDate={maxDate}
+      /> */}
+
       <InputLabel
         id="dropdown-label"
         label="ticket-type"
@@ -71,4 +107,37 @@ export default function TicketsSection() {
       />
     </>
   );
+}
+{
+  /* <TextField
+        className={styles.inputField}
+        type="date"
+        helperText="Choose a date"
+        value={formData.date}
+        required
+        onChange={(e) =>
+          dispatch({
+            //dispatch to the global formData obj. with new state value
+            action: "UPDATE_FIELD",
+            payload: { field: "date", value: e.target.value },
+          })
+        }
+     
+      /> */
+}
+{
+  /* <DatePicker
+        label="Select Date"
+        value={formData.date}
+        onChange={(e) =>
+          dispatch({
+            //dispatch to the global formData obj. with new state value
+            action: "UPDATE_FIELD",
+            payload: { field: "date", value: e.target.value },
+          })
+        }
+        renderInput={(params) => <TextField {...params} />}
+        minDate={minDate}
+        maxDate={maxDate}
+      > */
 }
